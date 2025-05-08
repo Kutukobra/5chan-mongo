9fd19e7b336380e2f562ab2344368fe1840fe4ef
Kutukobra <nadzhiff@gmail.com>
2025-05-08 07:31:37 +0700

    user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,52 +14,37 @@ exports.login = async (req, res) => {
         baseResponse(
             res,
             true,
-            200
-        )
-        res.status(200).json({ success: true, message: "Found user", data: user });
+            200,
+            "Login success.",
+            user
+        );
+    } catch (err) {
+        baseResponse(
+            res,
+            false,
+            400,
+            "Login failed: " + err.message
+        );
+        console.log(`Error Message: ${err.message}`);
+    }
+}
+
+exports.register = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = new User({ username: username, password: password });
+        await user.save();
+
+        baseResponse(
+            res,
+            true,
+            200,
+            "Register success.",
+            user
+        )            
     } catch (err) {
         res.status(400).json({ success: false, message: err.message });
         console.log(`Error Message: ${err.message}`);
     }
 }
-
-exports.registerCreator = async (req, res) => {
-    if (!req.query.name || !req.query.password) {
-        return baseResponse(
-            res,
-            false,
-            400,
-            "No username of password given."
-        )
-    }
-
-    try {
-        req.query.password = await password.hashPassword(req.query.password);
-
-        const creator = await creatorRepository.registerCreator(req.query);
-        
-        baseResponse(
-            res,
-            true,
-            200,
-            "Creator created.",
-            creator
-        );
-    } catch (error) {
-        if (error.constraint === 'creators_name_key') {
-            return baseResponse(
-                res,
-                false,
-                400,
-                "Username taken"
-            );
-        }
-        
-        baseResponse(
-            res,
-            false,
-            500,
-            error.message || "Failed to create creator"
-        )
-    }
-};
