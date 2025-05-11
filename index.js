5668a6594f1e7aee725489519e7c3ca9e9cff9a6
unknown <farrasbryan06@gmail.com>
2025-05-12 00:35:58 +0700

    connect DB bisa kayaknya

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ require('dotenv').config();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const { connectDB } = require('./src/database/mdb');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
 app.use(express.json());
-
-app.use(cors());  
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// const Recaptcha = require('express-recaptcha').RecaptchaV3;
-// const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
@@ -26,7 +22,8 @@ app.get('/', (req, res) => {
 app.use('/user', require('./src/routes/user.route'));
 app.use('/post', require('./src/routes/post.route'));
 
-app.listen(
-    PORT,
-    () => console.log(`Server running on http://localhost:${PORT}`)
-);
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+}).catch((err) => {
+    console.error("Failed to connect to database:", err);
+});
